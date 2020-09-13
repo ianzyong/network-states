e86ec4bc91ace6fa643be509fcf7dce2b26a914d
Ian Ong <ianzyong@gmail.com>
2020-09-13 10:35:46 -0400

    backwards compatibility

--- util/get_files.py
+++ util/get_files.py
@@ -16,7 +16,8 @@ def convertSeconds(time):
         minutes = "0" + str(minutes)
     if hours < 10:
         hours = "0" + str(hours)
-    return f"{hours}:{minutes}:{seconds}"
+    c = ":"
+    return c.join([hours,minutes,seconds])
 
 # input parameters
 username = input('IEEG.org username: ')
@@ -32,30 +33,30 @@ removed_channels = removed_channels.split(",")
 total_time = (stop_time_usec - start_time_usec)//1000000
 eeg_file_size = round((total_time/60)*(48962/1024**2),4)
 func_file_size = eeg_file_size/2
-answer = input(f"Duration requested = {convertSeconds(total_time)}. Estimated space required = {eeg_file_size} GB + {func_file_size} GB = {round(eeg_file_size + func_file_size,4)} GB.\nProceed? (y/n) ")
+answer = input("Duration requested = {}. Estimated space required = {} GB + {} GB = {} GB.\nProceed? (y/n) ".format(convertSeconds(total_time),eeg_file_size,func_file_size,round(eeg_file_size + func_file_size,4)))
 
 # download eeg data and calculate adjacency matrices
 if answer == 'y' or answer == 'Y':
     parent_directory = os.path.join('gdrive','public','DATA','Human_Data','BIDS_processed')
-    patient_directory = os.path.join(parent_directory,f"sub-{rid}")
+    patient_directory = os.path.join(parent_directory,"sub-{}".format(rid))
     eeg_directory = os.path.join(patient_directory,'eeg')
     functional_directory = os.path.join(patient_directory,'connectivity_matrices','functional','eeg')
     # create necessary directories if they do not exist
     if not os.path.exists(patient_directory):
         os.makedirs(eeg_directory)
         os.makedirs(functional_directory)
-    outputfile = os.path.join(eeg_directory,f"sub-{rid}_{iEEG_filename}_{start_time_usec}_{stop_time_usec}_EEG.pickle")
+    outputfile = os.path.join(eeg_directory,"sub-{}_{}_{}_{}_EEG.pickle".format(rid,iEEG_filename,start_time_usec,stop_time_usec))
     get_iEEG_data(username, password, iEEG_filename, start_time_usec, stop_time_usec, removed_channels, outputfile)
 
     # start timer
     start = time.time()
 
     func_inputfile = outputfile
-    func_outputfile = os.path.join(functional_directory,f"sub-{rid}_{iEEG_filename}_{start_time_usec}_{stop_time_usec}_functionalConnectivity.pickle")
+    func_outputfile = os.path.join(functional_directory,"sub-{}_{}_{}_{}_functionalConnectivity.pickle".format(rid,iEEG_filename,start_time_usec,stop_time_usec))
     get_Functional_connectivity(func_inputfile,func_outputfile)
 
     # stop timer
     end = time.time()
 
     # report time elapsed for functional connectivity calculation
-    print(f"Time elapsed = {convertSeconds(int(end - start))}")
+    print("Time elapsed = {}".format(convertSeconds(int(end - start))))
